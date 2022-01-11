48bf163ba233fb3e41cf89b9afd08076b3d07ea4
jasonv <jdvn2517@gmail.com>
2022-01-11 09:11:33 -0800

    first commit

--- week-4/d2-es6-classes/practices/basic-class-syntax/classes/person.js
+++ week-4/d2-es6-classes/practices/basic-class-syntax/classes/person.js
@@ -1,4 +1,21 @@
-// Your code here
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstname;
+    this.lastName = lastName;
+    this.age = age;
+    introduce();
+
+
+  }
+  // instance methods
+  introduce () {
+    console.log(`Hi I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`);
+  }
+
+  static introducePeople (...persons) {
+    console.log()
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
