832902451f0f9880cc9704c9819b97c74b66dd10
jasonv <jdvn2517@gmail.com>
2022-01-11 15:04:53 -0800

    friendly and evil dragon classes complete

--- week-4/d2-es6-classes/practices/practice-for-week-04-class-dragons-long-practice-main/classes/dragon.js
+++ week-4/d2-es6-classes/practices/practice-for-week-04-class-dragons-long-practice-main/classes/dragon.js
@@ -1,4 +1,34 @@
-// Your code here
+
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+    this.breathesFire();
+  }
+  breathesFire () {
+    return `${this.name} breathes fire everywhere! BURN!!!!`;
+  }
+
+  static getDragons (...dragons) {
+    // const dragonArray = [];
+    // dragonArray.push(dragons);
+    // return dragonArray;
+
+   return dragons.map(dragon => dragon.name) // also works
+  }
+}
+module.exports = Dragon
+// const puff = new Dragon("Puff", "green");
+// console.log(puff);
+// console.log(puff.breathesFire());
+
+// const toothless = new Dragon("Toothless", "black");
+// console.log(toothless);
+// console.log(toothless.breathesFire());
+
+const puff = new Dragon("Puff", "green");
+const toothless = new Dragon("Toothless", "black");
+console.log(Dragon.getDragons(puff, toothless));
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
