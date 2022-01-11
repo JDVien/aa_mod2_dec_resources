0caa6981a70c7509e740c7236f9fc7020a2d8958
jasonv <jdvn2517@gmail.com>
2022-01-11 13:33:50 -0800

    first commit

---
+++ week-4/d2-es6-classes/practices/practice-for-week-04-class-dragons-long-practice-main/all-dragons.js
@@ -0,0 +1,13 @@
+// Your code here All Deez Draggunzzz
+
+/****************************************************************************/
+/******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
+
+try {
+  module.exports = {
+    falkor,
+    smaug,
+  };
+} catch {
+  module.exports = null;
+}
