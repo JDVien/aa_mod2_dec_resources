2b2d66d9fc4d2e1f4f475bd2ffca69984928d4dd
jasonv <jdvn2517@gmail.com>
2022-01-11 13:29:36 -0800

    updated basic-class-syntax and common-js-modules

--- week-4/d2-es6-classes/practices/basic-class-syntax/classes/person.js
+++ week-4/d2-es6-classes/practices/basic-class-syntax/classes/person.js
@@ -1,22 +1,39 @@
 class Person {
   constructor(firstName, lastName, age) {
-    this.firstName = firstname;
+    this.firstName = firstName;
     this.lastName = lastName;
     this.age = age;
-    introduce();
 
 
-  }
+  };
   // instance methods
   introduce () {
     console.log(`Hi I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`);
   }
 
-  static introducePeople (...persons) {
-    console.log()
+  static introducePeople (people) {
+    if (!Array.isArray(people)) {
+      console.log("introducePeople only takes an array as an argument.")
+    // console.log(`error: introducePeople only takes an array as an argument`);
+    return;
+  }
+    for (let person of people) {
+      if (!(person instanceof Person)) {
+        console.log("All items in array must be Person class instances");
+        // console.log("All items in array must be instance of class Person");
+        return;
+      }
+    }
+    people.forEach((person) => person.introduce());
+
   }
 }
-
+// const jason = new Person('Jason', 'Vien', 29)
+// const jeff = new Person('Jeff', 'Granof', 100)
+// jason.introduce();
+// jeff.introduce()
+// const array = [jason, jeff]
+// Person.introducePeople(array)
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
--- week-4/d2-es6-classes/practices/common-js-modules/index.js
+++ week-4/d2-es6-classes/practices/common-js-modules/index.js
@@ -1,6 +1,8 @@
 const { sayHelloTo } = require('./send-messages/say-hello-to')
 const { giveMessageToMrsPotato } = require('./send-messages/give-message-to-mrs-potato')
 const {message1: msg1, message2: msg2, message3: msg3 } = require('./messages')
+
+// If you are pulling in files from the same folder you can use ('./ ) aka a single dot slash
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
