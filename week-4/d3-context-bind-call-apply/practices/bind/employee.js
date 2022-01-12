06bc9866f4ca9d3ef27a512e5b7cbd4b256331c7
jasonv <jdvn2517@gmail.com>
2022-01-12 09:12:45 -0800

    complete

--- week-4/d3-context-bind-call-apply/practices/bind/employee.js
+++ week-4/d3-context-bind-call-apply/practices/bind/employee.js
@@ -1,1 +1,19 @@
-// Your code here
+class Employee {
+    constructor (name, occupation) {
+        this.name = name;
+        this.occupation = occupation;
+    }
+    sayName() {
+        return console.log(`${this.name} says hello`);
+    }
+
+    sayOccupation() {
+        return console.log(`${this.name} is a ${this.occupation}`);
+    }
+}
+
+const emp = new Employee("Jason");
+const nameSay = emp.sayName.bind(emp)
+setTimeout(nameSay, 2000)
+
+module.exports = Employee
