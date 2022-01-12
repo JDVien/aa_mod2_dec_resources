70afc5829c69fd2bee99c1406812a62e66e12993
jasonv <jdvn2517@gmail.com>
2022-01-12 09:13:20 -0800

    complete

--- week-4/d3-context-bind-call-apply/practices/context-regular-functions/context.js
+++ week-4/d3-context-bind-call-apply/practices/context-regular-functions/context.js
@@ -16,22 +16,25 @@ const nemo = new Fish('Nemo');
 
 /********************************* Scenario 1 *********************************/
 // eat(); // ?
-
+// undefined eats fish food
 
 /********************************* Scenario 2 *********************************/
 // nemo.eat = eat;
 // nemo.eat(); // ?
-
+// output: Nemo eats fish food
+// remember that function eat() is not a Fish class instance
 
 /********************************* Scenario 3 *********************************/
 // nemo.eat = eat;
 // eat(); // ?
-
+// undefined eats fish food
 
 /********************************* Scenario 4 *********************************/
 // nemo.swim(); // ?
+// output: Nemo swimming in the water
 
 
 /********************************* Scenario 5 *********************************/
-// const swim = nemo.swim;
-// swim(); // ?
+const swim = nemo.swim;
+swim(); // ?
+// // throws an error
