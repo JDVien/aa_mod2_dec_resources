657815e4cd1da4012d865aa48a2c23801144eea2
jasonv <jdvn2517@gmail.com>
2022-01-13 13:28:24 -0800

    phase 2 push

--- week-4/d4-test-driven-development/practices/practice-for-week-04-tdd-style-long-practice-main/test/number-fun-spec.js
+++ week-4/d4-test-driven-development/practices/practice-for-week-04-tdd-style-long-practice-main/test/number-fun-spec.js
@@ -1,1 +1,17 @@
-// Your code here
+const { assert } = require('chai')
+const chai = require('chai')
+const expect = chai.expect
+const { returnsThree } = require('../problems/number-fun')
+const { reciprocal } = require('./problems/number-fun')
+
+describe("returnsThree()", () => {
+    it("should return the number 3", () => {
+        let test1 = 3;
+        let test2 = 4;
+        let test3 = 6;
+        assert.strictEqual(returnsThree(test1), 3)
+        assert.strictEqual(returnsThree(test2), 3)
+        assert.strictEqual(returnsThree(test3), 3)
+    })
+})
+
