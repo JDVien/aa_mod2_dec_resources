9de09e198418bf0e8bec62eb5020bcadf48d81b1
Jae Shin <7jaeshin@gmail.com>
2022-01-13 16:05:16 -0500

    reverse-string done

--- week-4/d4-test-driven-development/practices/practice-for-week-04-tdd-style-long-practice-main/problems/reverse-string.js
+++ week-4/d4-test-driven-development/practices/practice-for-week-04-tdd-style-long-practice-main/problems/reverse-string.js
@@ -1,3 +1,11 @@
-module.exports = function reverseString(string) {
-  // Your code here
+function reverseString(string) {
+  if (typeof string !== 'string') throw Error('this is not a string');
+
+  let newStr = string.split("")
+  return newStr.reverse().join('');
 };
+
+// reverseString(4);
+console.log(reverseString('hello world'))
+
+module.exports = { reverseString };
--- week-4/d4-test-driven-development/practices/practice-for-week-04-tdd-style-long-practice-main/test/reverse-string-spec.js
+++ week-4/d4-test-driven-development/practices/practice-for-week-04-tdd-style-long-practice-main/test/reverse-string-spec.js
@@ -1,1 +1,16 @@
-// Your code here
+const { assert } = require('chai')
+const chai = require('chai')
+const expect = chai.expect
+const { reverseString } = require('../problems/reverse-string')
+
+describe('reverseString(str)', () => {
+    it('should return a string', () => {
+        assert.strictEqual(typeof reverseString('jason'), 'string')
+    })
+    it('should return input string reversed', () => {
+        let test1 = 'jae'
+        let test2 = 'this is a string'
+        assert.strictEqual(reverseString(test1), 'eaj')
+        assert.strictEqual(reverseString(test2), 'gnirts a si siht')
+    })
+});
