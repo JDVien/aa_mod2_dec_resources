c15e227fdaceaea9bcd0c9d1c3d7dbbed9a1b5d1
jasonv <jdvn2517@gmail.com>
2022-01-13 16:36:46 -0800

    most recent commit, working on phase 3

--- week-4/d4-test-driven-development/practices/practice-for-week-04-tdd-style-long-practice-main/problems/my-map.js
+++ week-4/d4-test-driven-development/practices/practice-for-week-04-tdd-style-long-practice-main/problems/my-map.js
@@ -1,5 +1,16 @@
 function myMap(inputArray, callback) {
-  // Your code here
+  let newArray = [];
+  for (let index = 0; index < inputArray.length; index++) {
+    const element = inputArray[index];
+    newArray.push(callback(element));
+  }
+  return newArray;
 }
 
-module.exports = myMap;
+const arr = [1, 2, 3];
+const callback = (el) => el * 2;
+
+console.log(myMap(arr, callback)); // prints [2,4,6]
+console.log(arr); // prints [1,2,3]
+
+module.exports = { myMap }
--- week-4/d4-test-driven-development/practices/practice-for-week-04-tdd-style-long-practice-main/test/my-map-spec.js
+++ week-4/d4-test-driven-development/practices/practice-for-week-04-tdd-style-long-practice-main/test/my-map-spec.js
@@ -1,1 +1,23 @@
-// Your code here
+const { assert } = require('chai')
+const chai = require('chai')
+const expect = chai.expect
+const spies = require("chai-spies");
+chai.use(spies);
+const { myMap } = require("../problems/my-map");
+
+describe('myMap(inputArray, callback)', () => {
+    it('should not mutate the passed-in array argument', () => {
+        const test = [1, 2, 3];
+        const test2 = [7,6,5,4];
+        const callback = (element) => element * 2;
+        const actual1 = myMap(test, callback); // [2,4,6]
+        const expect1 = [1,2,3];
+
+        // assert.deepEqual(myMap(test, callback), [1,2,3]);
+        expect(actual1).to.eql(test);
+    })
+
+    it('should not call the built-in Array.map', () => {
+        expect()
+    })
+})
