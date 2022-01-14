71ca6d678f22903e830a91993bfdf3cc73e4b85e
jasonv <jdvn2517@gmail.com>
2022-01-13 16:56:38 -0800

    updated my-map/my-map spec with mutation test solution

--- week-4/d4-test-driven-development/practices/practice-for-week-04-tdd-style-long-practice-main/problems/my-map.js
+++ week-4/d4-test-driven-development/practices/practice-for-week-04-tdd-style-long-practice-main/problems/my-map.js
@@ -4,7 +4,9 @@ function myMap(inputArray, callback) {
     const element = inputArray[index];
     newArray.push(callback(element));
   }
-  return newArray;
+  // I intentionally mutated the original array here so that we could test to make sure. run this in node, and then npm-test the spec
+  inputArray.push(6)
+  return inputArray, newArray; // I returned both the newArray and the inputArray so that in the spec, the test = [1,2,3] variable will fail the test case
 }
 
 const arr = [1, 2, 3];
--- week-4/d4-test-driven-development/practices/practice-for-week-04-tdd-style-long-practice-main/test/my-map-spec.js
+++ week-4/d4-test-driven-development/practices/practice-for-week-04-tdd-style-long-practice-main/test/my-map-spec.js
@@ -4,20 +4,23 @@ const expect = chai.expect
 const spies = require("chai-spies");
 chai.use(spies);
 const { myMap } = require("../problems/my-map");
-
+beforeEach (() => {
+    const test = [1, 2, 3 ]
+})
 describe('myMap(inputArray, callback)', () => {
     it('should not mutate the passed-in array argument', () => {
         const test = [1, 2, 3];
-        const test2 = [7,6,5,4];
+        const testB = [7,6,5,4];
         const callback = (element) => element * 2;
         const actual1 = myMap(test, callback); // [2,4,6]
-        const expect1 = [1,2,3];
+        const expect1 = test;
 
-        // assert.deepEqual(myMap(test, callback), [1,2,3]);
-        expect(actual1).to.eql(test);
+        //  assert.deepEqual(myMap(test, callback), [1,2,3]);
+
+        expect(test).to.eql([1,2,3]); // this will fail with the changes I made to the myMap function
     })
 
     it('should not call the built-in Array.map', () => {
-        expect()
+        chai.spy.on(myMap);
     })
 })
