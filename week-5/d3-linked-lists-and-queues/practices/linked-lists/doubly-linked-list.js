600d76318681c611a00c38c1d491575f5d83fc68
jasonv <jdvn2517@gmail.com>
2022-01-19 11:11:55 -0800

    linkedlist practices update

--- week-5/d3-linked-lists-and-queues/practices/linked-lists/doubly-linked-list.js
+++ week-5/d3-linked-lists-and-queues/practices/linked-lists/doubly-linked-list.js
@@ -8,15 +8,39 @@ class DoublyLinkedListNode {
 
 class DoublyLinkedList {
   constructor() {
-    // Your code here
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
 
   addToHead(val) {
-    // Your code here
-  }
+    let newNode = new DoublyLinkedListNode(val);
 
+    // check if list has nodes already
+    if (this.length >= 1) {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    } else {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    this.length++
+  }
+  /// note for assessment:
   addToTail(val) {
     // Your code here
+    let newNode = new DoublyLinkedListNode(val);
+    // check to see if there's something in the list:
+    if (this.length >= 1) {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    } else { // if empty
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
   }
 
   // You can use this function to help debug
--- week-5/d3-linked-lists-and-queues/practices/linked-lists/linked-list.js
+++ week-5/d3-linked-lists-and-queues/practices/linked-lists/linked-list.js
@@ -2,20 +2,47 @@ class LinkedListNode {
   constructor(val) {
     this.value = val;
     this.next = null;
+
   }
 }
 
 class LinkedList {
   constructor() {
     // Your code here
+    // default to empty
+    this.head = null; // if head = null, list is empty
+    this.length = 0;
+    this.tail = null;
   }
 
-  addToHead(val) {
-    // Your code here
+  addToHead(val) { // 0(1) complexity
+    // creates a new node with given value
+    const newNode = new LinkedListNode(value);
+
+    // set the node's Next pointer to the list's current head point
+    newNode.next = this.head;
+
+    // set the list head to point to the new link
+    this.head = newNode;
+
+    this.length++;
   }
 
   addToTail(val) {
-    // Your code here
+
+    const newNode = new LinkedListNode(value);
+    this.length++; // needs to be incremented first
+    //this structure is an empty list
+    if (!this.head) {
+      this.head = newNode;
+      return this;
+    }
+    // now list with something in it
+    let curr = this.head // start at head
+    while(curr.next) { // as long as the next property is not null
+      curr = curr.next;
+    }
+    curr.next = newNode;
   }
 
   // You can use this function to help debug
