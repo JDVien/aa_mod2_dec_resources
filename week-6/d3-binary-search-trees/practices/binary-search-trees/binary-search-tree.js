4c9cef858a94e4a54fa62658b1a0fe9c30d7edd2
jasonv <jdvn2517@gmail.com>
2022-01-25 22:58:05 -0800

    bst passed 9 tests

--- week-6/d3-binary-search-trees/practices/binary-search-trees/binary-search-tree.js
+++ week-6/d3-binary-search-trees/practices/binary-search-trees/binary-search-tree.js
@@ -10,20 +10,57 @@ class TreeNode {
 class BinarySearchTree {
 
   constructor() {
-    // Your code here
+    this.root = null;
   }
 
   insert(val, currentNode=this.root) {
-    // Your code here
+    let newNode = new TreeNode(val);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    while (currentNode) {
+      if (val < currentNode.val) { // makes sure any val smaller than root.val goes to the left side of the tree
+        if (currentNode.left === null) {
+          currentNode.left = newNode; // sets the new value in new left node
+          return this;
+        }
+        currentNode = currentNode.left;
+      } else {  // does the same check for values larger than root.value, and puts them on the right of the tree
+        if (currentNode.right === null) {
+          currentNode.right = newNode;
+          return this;
+        }
+        currentNode = currentNode.right;
+      }
+    }
   }
 
   search(val) {
-    // Your code here
+
+    if (!this.root) return false;
+    let current = this.root
+    let found = false;
+    while (current && !found) {
+      if (current.val  > val) {
+        current = current.left;
+      } else if (current.val < val) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    if (!found)
+      return false;
+    return found;
   }
 
 
   preOrderTraversal(currentNode = this.root) {
-    // Your code here
+    // console.log(currentNode);
+
+    // this.preOrderTraversal(this.left)
+    // this. preOrderTraversal(this.right)
   }
 
 
